4e734d9d3e1046f7673374fdf032a6d47f19bc4f
SXXK_d01 <112852343+g0311@users.noreply.github.com>
2025-03-13 22:16:25 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+#define endl '\n'
+
+int DP[100];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string str;
+	cin >> str;
+	cout << str << "??!";
+
+	return 0;
+}
