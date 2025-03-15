b8bf2f82657a4788fbd1129f4943c6f91298d3f6
SXXK_d01 <112852343+g0311@users.noreply.github.com>
2025-03-15 18:24:03 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+#define endl '\n'
+
+vector<int> num;
+int visit[13];
+vector<int> output;
+
+void DFS(int id, int ct)
+{
+	if (ct == 6)
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			cout << output[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+	for (int i = id; i < num.size(); i++)
+	{
+		if (visit[i] == 0)
+		{
+			visit[i] = 1;
+			output.push_back(num[i]);
+			DFS(i, ct + 1);
+			output.pop_back();
+			visit[i] = 0;
+		}
+	}
+	return;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (true)
+	{
+		int k;
+		cin >> k;
+		if (k == 0)
+			return 0;
+
+		num.clear();
+		memset(visit, 0, sizeof(visit));
+		for (int i = 0; i < k; i++)
+		{
+			int no;
+			cin >> no;
+			num.push_back(no);
+		}
+		sort(num.begin(), num.end());
+
+		DFS(0,0);
+		cout << endl;
+	}
+
+	return 0;
+}
