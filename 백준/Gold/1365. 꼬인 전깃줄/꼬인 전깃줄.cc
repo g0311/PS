c792fd297a68828d36da0f949b3737364c3b4d76
SXXK_d01 <112852343+g0311@users.noreply.github.com>
2025-04-12 17:59:26 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 12 ms, Memory: 2924 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define INF 2e9
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	
+	vector<int> arr(N);
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+	
+	vector<int> DP;
+	for (int i = 0; i < N; i++)
+	{
+		auto iter = lower_bound(DP.begin(), DP.end(), arr[i]);
+		if (iter == DP.end())
+		{
+			DP.push_back(arr[i]);
+		}
+		else
+		{
+			*iter = arr[i];
+		}
+	}
+	
+	cout << N - DP.size();
+
+	return 0;
+}
+
